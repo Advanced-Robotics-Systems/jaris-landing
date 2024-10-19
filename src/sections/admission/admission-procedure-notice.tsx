a4e15485fa1bb2377022fac96b51592405da79d0
Ishmum <ishmum20000@gmail.com>
2024-10-19 23:19:29 +0800

    Complete notice section

--- src/sections/admission/admission-procedure-notice.tsx
+++ src/sections/admission/admission-procedure-notice.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const AdmissionProcedureNotice = () => {
-  return (
-    <div>AdmissionProcedureNotice</div>
-  )
-}
+	return (
+		<div className="w-full bg-white/80 padding sm:px-8 md:px-16 lg:px-56 xl:px-72 text-center">
+			<p className="text-base sm:text-sm md:text-lg lg:text-base text-jaris-black lg:leading-7">
+				New openings to all grades are announced every academic year prior to
+				school closure for summer vacation. However, the Principal may consider
+				granting admissions on reserved seats to those deserving students who
+				will be joining from abroad any time of the year, provided Ministry of
+				Education (Department of Foreign and National Education) approves such
+				admission.
+			</p>
+		</div>
+	);
+};
 
-export default AdmissionProcedureNotice
+export default AdmissionProcedureNotice;
