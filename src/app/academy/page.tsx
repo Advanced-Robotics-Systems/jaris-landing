8ad36274c4dfedec13802086a5ca5fb95a9ce34e
Ishmum <ishmum20000@gmail.com>
2024-10-30 16:00:05 +0800

    Set up remaining pages

---
+++ src/app/academy/page.tsx
@@ -0,0 +1,17 @@
+import { Footer, Header, NavbarPlaceholder } from '@/sections'
+import React from 'react'
+
+const Academy = () => {
+  return (
+    <>
+      <Header />
+      <NavbarPlaceholder />
+      <main>
+        
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Academy
---
+++ src/app/academy/special-needs/page.tsx
@@ -0,0 +1,17 @@
+import { Footer, Header, NavbarPlaceholder } from '@/sections'
+import React from 'react'
+
+const SpecialNeeds = () => {
+  return (
+    <>
+      <Header />
+      <NavbarPlaceholder />
+      <main>
+
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default SpecialNeeds
