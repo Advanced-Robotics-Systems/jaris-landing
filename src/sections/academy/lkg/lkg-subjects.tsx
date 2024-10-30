b43ab6fa211138d3c7c61e959c98633bbfe6c372
Ishmum <ishmum20000@gmail.com>
2024-10-31 03:30:05 +0800

    Update spacing

--- src/sections/academy/lkg/lkg-subjects.tsx
+++ src/sections/academy/lkg/lkg-subjects.tsx
@@ -9,20 +9,20 @@ import {
 
 const LKGSubjects = () => {
 	return (
-		<div className="padding bg-jaris-blue-light lg:px-36 xl:px-48 2xl:px-62">
+		<div className="padding-y bg-jaris-blue-light px-6 md:px-18 lg:px-36 xl:px-48 2xl:px-62">
 			<h2 className="heading-secondary font-bold text-jaris-gold mb-6 uppercase">
 				Subjects
 			</h2>
 
 			{/* Table Section */}
-			<div className="mb-8 bg-white/60 rounded-lg p-2 lg:p-10">
-				<h2 className="heading-secondary text-jaris-gold-dark mb-6">
+			<div className="mb-6 lg:mb-16 bg-white/60 rounded-lg py-2 lg:p-10">
+				<h2 className="heading-secondary text-jaris-gold-dark mb-3 lg:mb-8 max-sm:px-3">
 					English and Mathematics
 				</h2>
 				<Table
 					aria-label="LKG Subjects"
 					hideHeader
-					className="w-full lg:w-[70%] m-auto"
+					className="w-full lg:w-[70%] m-auto text-jaris-blue"
 				>
 					<TableHeader>
 						<TableColumn className="text-jaris-blue">
@@ -76,21 +76,21 @@ const LKGSubjects = () => {
 			</div>
 
 			<div className="flex max-lg:flex-col gap-6 lg:gap-16">
-				<div className="flex-1 flex flex-col p-1 lg:p-6 bg-white/60 shadow-lg rounded-lg">
+				<div className="flex-1 flex flex-col p-3 lg:p-6 bg-white/60 shadow-lg rounded-lg gap-3 lg:gap-8">
 					<h3 className="heading-secondary text-jaris-gold-dark">
 						Arabic and Islamic Studies
 					</h3>
-					<ol className="list-decimal pl-5 mt-4">
+					<ol className="list-decimal pl-5 text-jaris-blue">
 						<li className="subtitle">Recognize and read Arabic alphabet</li>
 						<li className="subtitle">Simple supplications</li>
 					</ol>
 				</div>
 
-				<div className="flex-1 flex flex-col p-1 lg:p-6 bg-white/60 shadow-lg rounded-lg">
+				<div className="flex-1 flex flex-col p-3 lg:p-6 bg-white/60 shadow-lg rounded-lg gap-3 lg:gap-8">
 					<h3 className="heading-secondary text-jaris-gold-dark">
 						General Knowledge
 					</h3>
-					<p className="subtitle mt-4">
+					<p className="subtitle text-jaris-blue">
 						The children are groomed to respect and be respected and valued and
 						are also expected to be involved with both the school and the
 						classroom. They are taught about self-care, cultural values, and
