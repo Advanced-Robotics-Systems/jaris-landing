8cd7db236815a1e1f2341db0407076b34490e4f3
Ishmum <ishmum20000@gmail.com>
2024-11-05 04:44:47 +0800

    Fix responsive layout

--- src/sections/extracurricular/extracurricular-cta.tsx
+++ src/sections/extracurricular/extracurricular-cta.tsx
@@ -4,9 +4,9 @@ import { Button, Card, Image } from "@nextui-org/react";
 const ExtracurricularCTA = ({description, imgSrc} : {description: string, imgSrc: string}) => {
 	return (
 		<div className="bg-bg-primary padding lg:px-36 xl:px-48">
-			<Card className=" bg-jaris-blue text-jaris-white shadow-lg flex-row flex-wrap h-full lg:pl-10">
+			<Card className=" bg-jaris-blue text-jaris-white shadow-lg flex-row flex-wrap h-full lg:pl-10 py-4">
 				{/* Left Side: Text and Button */}
-				<div className="flex flex-col justify-between w-2/3 p-6">
+				<div className="flex flex-col justify-between sm:w-2/3 p-6">
 					<div>
 						<h3 className="heading-secondary font-semibold">Join us</h3>
 						<p className="subtitle mt-4">
@@ -16,17 +16,17 @@ const ExtracurricularCTA = ({description, imgSrc} : {description: string, imgSrc
 					<div className="mt-6 flex gap-4">
 						<Button
 							size="lg"
+              variant="solid"
 							as="a"
 							href="/contact"
-							radius="full"
-							className="bg-jaris-gold text-jaris-white"
+							className="bg-jaris-gold rounded-md lg:px-8"
 						>
-							Contact Us
+							<span className="subtitle text-jaris-blue font-semibold">Contact Us</span>
 						</Button>
 					</div>
 				</div>
 				{/* Right Side: Image */}
-				<div className="w-1/3 flex justify-center items-center p-4">
+				<div className="max-sm:hidden w-1/3 flex justify-center items-center p-4">
 					<Image
 						src={imgSrc}
 						alt="Join Us Placeholder Image"
