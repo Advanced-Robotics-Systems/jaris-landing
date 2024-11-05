0659862b043851beca59b812b096e899dcc737e6
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-11-05 13:14:13 +0600

    Renamed gallery to news, gallery will be a different route

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -95,9 +95,9 @@ import SpNeedsOverview1 from "./special-needs/sp-needs-overview-1";
 import SpNeedsOverview2 from "./special-needs/sp-needs-overview-2";
 
 // Gallery & News
-import GalleryCarousel from "./gallery/gallery-carousel";
-import News from "./gallery/news";
-import NewsBanner from "./gallery/news-banner";
+import GalleryCarousel from "./news/gallery-carousel";
+import News from "./news/news";
+import NewsBanner from "./news/news-banner";
 
 export {
   Header,
