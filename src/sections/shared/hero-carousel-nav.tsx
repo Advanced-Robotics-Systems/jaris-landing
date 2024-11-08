1513ea8ea5bb1873f7c6e38687b07d375762661d
Ishmum <ishmum20000@gmail.com>
2024-11-09 03:04:52 +0800

    Make carousel size responsive

--- src/sections/shared/hero-carousel-nav.tsx
+++ src/sections/shared/hero-carousel-nav.tsx
@@ -20,7 +20,7 @@ const HeroCarouselNav = ({
 
 	return (
 		<div className="relative w-full lg:h-[calc(100vh-70px)] padding-b lg:pb-36 bg-jaris-blue">
-			<div className="relative z-20 w-full flex flex-col items-center justify-center gap-44">
+			<div className="relative z-20 w-full h-full flex flex-col items-center justify-center gap-44">
 				<Swiper
 					slidesPerView={"auto"}
 					centeredSlides={true}
@@ -32,12 +32,12 @@ const HeroCarouselNav = ({
 					spaceBetween={70}
 					loop={true}
 					modules={[Autoplay, Pagination]}
-					className="mySwiper cta-swiper w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[700px]"
+					className="mySwiper cta-swiper w-full h-full"
 				>
 					{carouselItems.map((item: any, index: number) => (
 						<SwiperSlide
 							key={index}
-							className="!w-[90%] md:!w-[55%] !h-[85%] md:!h-[90%]"
+							className="!w-[90%] md:!w-[55%] !h-[80%] md:!h-[85%]"
 						>
 							<div className="relative h-full w-full overflow-hidden rounded-xl">
 								<div
@@ -61,7 +61,7 @@ const HeroCarouselNav = ({
 			</div>
 
 			{/* Button cards */}
-			<div className="absolute w-full h-28 sm:h-48 bottom-4 md:bottom-10 lg:bottom-16 xl:bottom-20 flex items-center justify-around z-30">
+			<div className="absolute w-full h-28 md:h-38 lg:h-44 xl:h-48 bottom-4 md:bottom-10 lg:bottom-16 xl:bottom-20 flex items-center justify-around z-30">
 				{carouselNavItems.map((item: any, index: number) => (
 					<Card
 						key={index}
@@ -71,7 +71,7 @@ const HeroCarouselNav = ({
 					>
 						<CardBody className="items-center justify-center overflow-hidden">
               <p className="small-text text-white">{item.subtitle}</p>
-              <p className="subtitle xl:heading-secondary font-semibold text-jaris-white">{item.title}</p>
+              <p className="w-full text-center subtitle xl:heading-secondary font-semibold text-jaris-white">{item.title}</p>
             </CardBody>
 					</Card>
 				))}
