20e24ffa6bb48dae5c5e7e4fee3cf5b1bc94f11d
Abrar Hussain Marzuk <abrar.16@protonmail.com>
2024-11-14 15:15:18 +0600

    Changes before JARIS meeting

--- src/sections/home/discover.tsx
+++ src/sections/home/discover.tsx
@@ -2,28 +2,28 @@
 import { motion } from "framer-motion";
 
 const Discover = () => {
-	return (
-		<div className=" h-screen max-md:h-[50vh] bg-bg-primary padding flex flex-col items-center justify-center text-jaris-blue font-[600]">
-			<motion.p
-				initial={{ opacity: 0, x: -20 }}
-				whileInView={{ opacity: 1, x: 0 }}
-				transition={{ duration: 0.3, ease: "easeInOut" }}
-				viewport={{once: true}}
-				className="heading-md"
-			>
-				Discover a community of
-			</motion.p>
-			<motion.p
-				initial={{ opacity: 0, x: 20 }}
-				whileInView={{ opacity: 1, x: 0 }}
-				transition={{ duration: 0.3, delay: 0.3, ease: "easeInOut" }}
-				viewport={{once: true}}
-				className="heading-md underline-animated"
-			>
-				unlimited possibility.
-			</motion.p>
-		</div>
-	);
+  return (
+    <div className=" h-screen max-md:h-[50vh] bg-bg-primary padding flex flex-col items-center justify-center text-jaris-blue font-[600]">
+      <motion.p
+        initial={{ opacity: 0, x: -20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        viewport={{ once: true }}
+        className="heading-md"
+      >
+        Discover a world of
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0, x: 20 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3, delay: 0.3, ease: "easeInOut" }}
+        viewport={{ once: true }}
+        className="heading-md underline-animated"
+      >
+        endless potential.
+      </motion.p>
+    </div>
+  );
 };
 
 export default Discover;
