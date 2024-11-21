0c8aaf7a385ed4e7c499a9841aea1eeb5cb11e8d
Ishmum <ishmum20000@gmail.com>
2024-11-21 20:10:08 +0800

    Align bottom texts to start

--- src/sections/home/hero.tsx
+++ src/sections/home/hero.tsx
@@ -35,7 +35,9 @@ const Hero = () => {
 					</SwiperSlide>
 				))}
 			</Swiper>
-			<div className="absolute w-full bottom-2 md:bottom-4 lg:bottom-8 px-6 lg:px-10 z-10 flex items-center justify-between">
+
+			{/* Hero bottom texts */}
+			<div className="absolute w-full bottom-2 md:bottom-4 lg:bottom-8 px-6 lg:px-10 z-10 flex items-start justify-between">
 				<div className="small-text md:subtitle text-jaris-white w-[40%] lg:w-[20%]">
 					An independent, college preparatory day school for grades KG to 12,
 					with separate campuses for boys and girls.
