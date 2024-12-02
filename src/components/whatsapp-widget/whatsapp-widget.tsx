cb9e47761be6f80e663d57bb26189222a5b4dbf5
Ishmum <ishmum20000@gmail.com>
2024-12-02 20:53:09 +0800

    Dissappear at screen top

--- src/components/whatsapp-widget/whatsapp-widget.tsx
+++ src/components/whatsapp-widget/whatsapp-widget.tsx
@@ -3,39 +3,64 @@ import { whatsappData } from "@/data";
 import { ICONS } from "@/utils/icons";
 import { Button, Link } from "@nextui-org/react";
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const WhatsappWidget = () => {
+	const [showWidget, setShowWidget] = useState(false);
 	const [isPanelOpen, setIsPanelOpen] = useState(false);
 
 	const togglePanel = () => {
 		setIsPanelOpen(!isPanelOpen);
 	};
 
+	const checkScroll = () => {
+		if (window.scrollY > window.innerHeight * 0.2) {
+			setShowWidget(true);
+		} else {
+			setShowWidget(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", checkScroll);
+
+		return () => {
+			window.removeEventListener("scroll", checkScroll);
+		};
+	}, []);
+
 	return (
 		<>
-			<motion.div
-				className="fixed bottom-5 right-5 z-50"
-				animate={{ rotate: isPanelOpen ? 180 : 0 }}
-				transition={{ duration: 0.5 }}
-			>
-				<Button
-					size="lg"
-					color="success"
-					isIconOnly
-					radius="full"
-					onPress={togglePanel}
-					startContent={
-						isPanelOpen ? (
-							<span className="text-jaris-white text-2xl">{ICONS.close}</span>
-						) : (
-							<span className="text-jaris-white text-2xl">
-								{ICONS.whatsapp}
-							</span>
-						)
-					}
-				/>
-			</motion.div>
+			<AnimatePresence>
+				{showWidget && (
+					<motion.div
+						className="fixed bottom-5 right-5 z-50"
+						initial={{ opacity: 0, rotate: 0 }}
+						animate={{ opacity: 1, rotate: isPanelOpen ? 180 : 0 }}
+						exit={{ opacity: 0, rotate: 0 }}
+						transition={{ duration: 0.5 }}
+					>
+						<Button
+							size="lg"
+							color="success"
+							isIconOnly
+							radius="full"
+							onPress={togglePanel}
+							startContent={
+								isPanelOpen ? (
+									<span className="text-jaris-white text-2xl">
+										{ICONS.close}
+									</span>
+								) : (
+									<span className="text-jaris-white text-2xl">
+										{ICONS.whatsapp}
+									</span>
+								)
+							}
+						/>
+					</motion.div>
+				)}
+			</AnimatePresence>
 
 			{/* Slide content */}
 			<AnimatePresence>
