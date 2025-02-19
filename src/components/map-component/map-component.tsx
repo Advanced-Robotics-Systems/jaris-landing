48c70f11fea3ba844a1351e9ce410eb30d1b6654
abrar-16m <abrar.16@protonmail.com>
2025-02-19 11:10:30 +0300

    Added fly to feature when button clicked

--- src/components/map-component/map-component.tsx
+++ src/components/map-component/map-component.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Button, ButtonGroup } from "@nextui-org/react";
-import { Map, Marker } from "@vis.gl/react-maplibre";
-import { useEffect, useState } from "react";
+import { Map, Marker, useMap } from "@vis.gl/react-maplibre";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import "maplibre-gl/dist/maplibre-gl.css";
 
@@ -11,48 +11,40 @@ const locations = [
     name: "1st JARIS",
     latitude: 24.67376139412055,
     longitude: 46.72046574868544,
-    // latitude: 24.673761,
-    // longitude: 46.720466,
   },
   {
     name: "2nd JARIS",
     latitude: 24.666693923205354,
     longitude: 46.70306593318544,
-    // latitude: 24.666694,
-    // longitude: 46.703066,
   },
   {
     name: "3rd JARIS",
     latitude: 24.666759071766748,
     longitude: 46.70879792698207,
-    // latitude: 24.666759,
-    // longitude: 46.708798,
   },
 ];
 
 const MapComponent = () => {
-  const [viewState, setViewState] = useState({
-    latitude: locations[0].latitude,
-    longitude: locations[0].longitude,
-    zoom: 16,
-  });
-
   const [currentSchool, setCurrentSchool] = useState(0);
+  const [isSame, setIsSame] = useState(false);
 
-  useEffect(() => {
-    setViewState({
-      latitude: locations[currentSchool].latitude,
-      longitude: locations[currentSchool].longitude,
-      zoom: 16,
-    });
-  }, [currentSchool]);
+  const handlePress = (x: number) => {
+    if (x == currentSchool) {
+      setIsSame(true);
+    } else {
+      setCurrentSchool(x);
+    }
+  };
 
   return (
-    <div className="w-full md:w-1/2 flex flex-col gap-y-3 bg-bg-text px-2 pt-3 pb-8 border border-black">
+    <div className="w-full md:w-1/2 flex flex-col gap-y-3 bg-bg-text px-2 pt-3 pb-8">
       <div className="w-full h-[500px]">
         <Map
-          {...viewState}
-          onMove={(e) => setViewState(e.viewState)}
+          initialViewState={{
+            latitude: locations[0].latitude,
+            longitude: locations[0].longitude,
+            zoom: 16,
+          }}
           mapStyle="https://tiles.openfreemap.org/styles/liberty"
           attributionControl={false}
           style={{
@@ -63,11 +55,12 @@ const MapComponent = () => {
           }}
         >
           {locations.map((item, index) => (
-            <Marker
+            <CustomMarker
               key={index}
-              longitude={item.longitude}
-              latitude={item.latitude}
-              color="black"
+              item={item}
+              currentS={currentSchool}
+              isS={isSame}
+              setIsS={setIsSame}
             />
           ))}
         </Map>
@@ -76,25 +69,59 @@ const MapComponent = () => {
         <ButtonGroup>
           <Button
             className="flex items-center justify-center bg-jaris-white text-jaris-blue text-xs text-center font-bold h-full w-28 cursor-pointer hover:bg-black/20 transition-background"
-            onPress={() => setCurrentSchool(0)}
+            onPress={() => handlePress(0)}
           >
             1st JARIS
           </Button>
           <Button
             className="flex items-center justify-center bg-jaris-white text-jaris-blue text-xs text-center font-bold h-full w-28 cursor-pointer hover:bg-black/20 transition-background"
-            onPress={() => setCurrentSchool(1)}
+            onPress={() => handlePress(1)}
           >
             2nd JARIS
           </Button>
           <Button
             className="flex items-center justify-center bg-jaris-white text-jaris-blue text-xs text-center font-bold h-full w-28 cursor-pointer hover:bg-black/20 transition-background"
-            onPress={() => setCurrentSchool(2)}
+            onPress={() => handlePress(2)}
           >
             3rd JARIS
           </Button>
         </ButtonGroup>
       </div>
     </div>
+  );
+};
+
+const CustomMarker = ({
+  item,
+  currentS,
+  isS,
+  setIsS,
+}: {
+  item: { latitude: number; longitude: number };
+  currentS: number;
+  isS: boolean;
+  setIsS: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const { current: map } = useMap();
+
+  useEffect(() => {
+    if (!map) return;
+    if (!isS) {
+      map.flyTo({
+        center: [locations[currentS].longitude, locations[currentS].latitude],
+        zoom: 16,
+      });
+    } else {
+      map.flyTo({
+        center: [locations[currentS].longitude, locations[currentS].latitude],
+        zoom: 16,
+      });
+      setIsS(false);
+    }
+  }, [map, currentS, isS]);
+
+  return (
+    <Marker longitude={item.longitude} latitude={item.latitude} color="black" />
   );
 };
 
